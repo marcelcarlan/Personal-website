e35c7ad4b4ca14fd020740262cf4ba2d9244f6ad
Marcel Carlan <33603076+marcelcarlan@users.noreply.github.com>
2019-04-15 03:35:20 +0300

    Add files via upload

---
+++ Website/cvFinal/cv/javascript/languageDictionary.js
@@ -0,0 +1,51 @@
+var arrLang = {
+    'en' : {
+        'home' : 'Home',
+        'download' : 'Download CV',
+        'aboutMe' : 'About me',
+        'describeMe' : 'My name is Cârlan Marcel-Ionuț, I am 21 years old and I am a student, second year, in Cluj-Napoca, at University of Mathematics and Computer Science, UBB, department of Computer Science. ' +
+        'I went to National College &#34;Radu Negru&#34; Făgăraș, ending with grade 9.05 at baccalaureate exam (with grade 10 at Computer Science). Also, during highschool I got grade C1 at' +
+        '<a class="opacity" target="_blank" href="https://drive.google.com/file/d/0B0-PNAVhQrMgMnFZaDk4TjlpTFE/view?usp=sharing"> &#34;Certificate in Advanced English&#34;</a> exam.',
+        'describeMe2' : 'At the moment,I am having fun learning at university.I am learning things like Web Development, Programming and Design Methods, Artificial Intelligence and many others. My favourite programming languages are: Java (especially JavaFX), Javascript, HTML, CCS, SQL și C#.',
+        'describeMe3' : 'Drawing represent my passion. In my spare time I do graphic design, especially digital design, using Adobe Illustrator and Photoshop.',
+        'myAbility' : 'My abilities',
+        'inProgramming' : 'In programming',
+        'inDesign' : 'In design',
+        'inSocial' : 'In social life',
+        'comunication' : 'Communication',
+        'teamSpirit':'Teamwork',
+        'creativity' : 'Creativity',
+        'contactData': 'Contacts',
+        'email':'Email: cmarcel97@gmail.com',
+        'phone' : 'Phone: 0758426882',
+        'location' : 'Location: Cluj-Napoca, Romania',
+        'myPortofolio' : 'My portfolio'
+    },
+    'ro' : {
+        'home' : 'Acasă',
+        'download' : 'Descarcă CV',
+        'aboutMe' : 'Despre mine',
+        'describeMe' : 'Mă numesc Cârlan Marcel-Ionuț, am 21 de ani și în momentul de față sunt student, anul II, în Cluj-Napoca, la Facultatea de Matematică-Informatică, din cadrul UBB, specializarea Informatică.\n' +
+        '      Am urmat  Colegiu Național &#34;Radu Negru&#34; Făgăraș, finalizându-l cu media de bacalaureat 9.05 (cu nota 10 la informatică). De asemenea, în timpul liceului am obținut calificativul C1 la examenul\n' +
+        '      <a class="opacity" target="_blank" href="https://drive.google.com/file/d/0B0-PNAVhQrMgMnFZaDk4TjlpTFE/view?usp=sharing">\n' +
+        '    &#34;Certificate in Advanced English&#34;.\n' +
+        '    </a>',
+        'describeMe2' : 'Actualmente, mă bucur de ceea ce învăț la facultate, precum Programare Web, Metode de Programare și Proiectare, Inteligență Artificială, dar și multe altele. Printre limbajelele de programare preferate se regăsesc: Java (în special JavaFX), Javascript, HTML, CCS, SQL și C#.',
+        'describeMe3' : 'Desenul reprezintă una dintre pasiunile mele, iar în timpul liber mă ocup de design grafic, în special, design digital, folosind Adobe Illustrator și Photoshop.',
+        'myAbility' : 'Abilitățile mele',
+        'inProgramming' : 'În programare',
+        'inDesign' : 'În design',
+        'inSocial' : 'În viața socială',
+        'comunication' : 'Comunicare',
+        'teamSpirit':'Spirit de echipă',
+        'creativity' : 'Creativitate',
+        'contactData': 'Date de contact',
+        'email':'Email: cmarcel97@gmail.com',
+        'phone' : 'Telefon: 0758426882',
+        'location' : 'Locație: Cluj-Napoca, Romania',
+        'myPortofolio' : 'Portofoliul meu'
+    }
+};
+
+var languageArrays={'ro':[["Salut!","Numele meu este","Cârlan Marcel Ionuț"],["Și sunt","Student și programator (în devenire) "]],
+                    'en':[["Hi!", "My name is", "Cârlan Marcel Ionuț "],["And I am a", "Student and programmer "]]};
